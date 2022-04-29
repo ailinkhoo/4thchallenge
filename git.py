773b2eeb0aef01eb3cbf9c67bcb13b85dfcf1f84
khooailin <ailinkhoo11@gmail.com>
2022-04-29 14:48:57 +0800

    Update every tuesday

--- git.py
+++ git.py
@@ -15,6 +15,7 @@ cmd = 'git add . && git commit -m "Update every tuesday" && git push'
 
 def update_github():
 	os.system(cmd)
+	os.system()
 
 # Task scheduling
 # Every tuesday at 20:00 update_github() is called
