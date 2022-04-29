d2273346a0b6fb890bdd36bf835cc8db7c3a2f36
khooailin <ailinkhoo11@gmail.com>
2022-04-29 14:53:33 +0800

    Update every tuesday

--- git.py
+++ git.py
@@ -17,11 +17,11 @@ def update_github():
 	os.system(cmd)
 	os.system('date')
 
-# Task scheduling
+# Task scheduling: https://www.geeksforgeeks.org/python-schedule-library/
 # Every tuesday at 20:00 update_github() is called
 # schedule.every().tuesday.at("20:00").do(update_github)
 
-schedule.every(2).minutes.do(update_github)
+schedule.every(1).minutes.do(update_github)
 
 # Loop so that the scheduling task
 # keeps on running all time.
