73da9836ee2e76a127a7859eb0e82cadcfd5ae9d
khooailin <ailinkhoo11@gmail.com>
2022-04-29 15:09:34 +0800

    Update every tuesday

--- git.py
+++ git.py
@@ -4,6 +4,7 @@
 import os
 import schedule
 import time
+from datetime import datetime
 
 # Command to execute
 # Using Windows OS command
@@ -15,13 +16,13 @@ cmd = 'git add . && git commit -m "Update every tuesday" && git push'
 
 def update_github():
 	os.system(cmd)
-	print(time())
+	print(datetime.now())
 
 # Task scheduling: https://www.geeksforgeeks.org/python-schedule-library/
 # Every tuesday at 20:00 update_github() is called
 # schedule.every().tuesday.at("20:00").do(update_github)
 
-schedule.every(2).minutes.do(update_github)
+schedule.every(1).minutes.do(update_github)
 
 # Loop so that the scheduling task
 # keeps on running all time.
